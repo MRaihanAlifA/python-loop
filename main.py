8c4e421b3a66e8e5d39f9a8656a8a672f19715c4
MRaihanAlifA <muhammadraihanalif@gmail.com>
2022-02-14 13:28:39 +0700

    adding code

---
+++ main.py
@@ -0,0 +1,10 @@
+
+book_count = 10
+
+print('Mom tell me to read all the books')
+
+book_read_count = 0
+
+for book_read_count in range (1 , book_count+1):
+    print(f'{book_read_count} book is being read')
+    book_read_count += 1
