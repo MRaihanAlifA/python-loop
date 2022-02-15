742f075c6d050eecf446a4a90ca921414175ed79
MRaihanAlifA <muhammadraihanalif@gmail.com>
2022-02-15 23:28:54 +0700

    adding code

--- undefined_count.py
+++ undefined_count.py
@@ -13,16 +13,20 @@ for book_read_count in range (1 , book_count+1):
 """
 while book_read_and_understand_count < book_count:
     attempt_read += 1
-    print(attempt_read)
+    '''print(attempt_read)'''
     if book_read_and_understand_count == 9:
-        print(f'i cant understand the {book_read_and_understand_count} book')
+        print(f'i cant understand the {book_read_and_understand_count + 1} book')
     else:
         book_read_and_understand_count += 1
         print(f'{book_read_and_understand_count} book is being read and understand')
         attempt_read = 0
     if attempt_read == 3:
+        """
         book_read_and_understand_count += 1
         print(f'{book_read_and_understand_count} book is being read and understand')
+        """
+        break
+
 
 
 print(' ')
